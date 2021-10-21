25ea0eaee35db14a593ddf0ed66087d37c11248b
Marcio Belloni <prof.belloni@gmail.com>
2021-10-20 23:35:46 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GameTOP
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        var jogo = new JogoFODA("Ronaldo");
+        jogo.IniciarJogo(); 
+        }
+    }
+
+    class JogoFODA{
+        private readonly string _NomeJogador;
+
+        public JogoFODA(string nome){
+            _NomeJogador = nome;
+        }
+                public void IniciarJogo(){
+            Console.Write($"{_NomeJogador} deu um passe.");
+        }
+    }
+}
